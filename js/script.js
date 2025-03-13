695f6355833c63163dfb8c3d2a2ab260054b639d
Rayam Fonsêca <133929446+VoupraSP@users.noreply.github.com>
2025-03-13 08:52:26 -0300

    adding function ativaParametros

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log('test');
+// Ativa itens navegacao
+const links = document.querySelectorAll('.header-menu a');
+
+function ativaLinks(link) {
+    const url = window.location.href;
+    const href = link.href;
+
+    if (url.includes(href)) {
+        link.classList.add("ativo");
+    }
+}
+
+links.forEach(ativaLinks);
+
+
+// Ativa itens orcamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativaParametro(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativaParametro);
